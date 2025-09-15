e010c058f40c9f6a74817a7c8a6a0ec9e74798cb
Zaclabit <zbranden6@gmail.com>
2025-09-15 14:44:08 -0400

    add progress table

--- database-files/Pandployer.sql
+++ database-files/Pandployer.sql
@@ -261,6 +261,30 @@ LOCK TABLES `Offer_Status` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `Progress`
+--
+
+DROP TABLE IF EXISTS `Progress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Progress` (
+  `crn` int NOT NULL,
+  `group_id` int NOT NULL,
+  `step` enum('job_description','res_1','res_2','interview','offer') DEFAULT NULL,
+  PRIMARY KEY (`crn`,`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Progress`
+--
+
+LOCK TABLES `Progress` WRITE;
+/*!40000 ALTER TABLE `Progress` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Progress` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Resume`
 --
 
@@ -418,4 +442,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-11  9:07:29
+-- Dump completed on 2025-09-15 14:41:29
