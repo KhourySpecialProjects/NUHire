81aafe83c7812e4c7c8888df2514a3c1291a2265
Zaclabit <zbranden6@gmail.com>
2025-09-30 16:25:17 -0400

    fix route

--- api/server.js
+++ api/server.js
@@ -2084,7 +2084,7 @@ app.post("/offers", (req, res) => {
 });
 
 // GET - Teachers view pending offers for their class
-app.get("/offers/:class_id", (req, res) => {
+app.get("/offers/class/:class_id", (req, res) => {
   const { class_id } = req.params;
   
   console.log("Fetching pending offers for class:", class_id);
