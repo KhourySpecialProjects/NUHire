fecc1edef9d01bbd70076eac3e13b93467be5e71
Zaclabit <zbranden6@gmail.com>
2025-10-09 11:36:18 -0400

    fix dashboard link

--- frontend/src/app/advisor-dashboard/page.tsx
+++ frontend/src/app/advisor-dashboard/page.tsx
@@ -108,7 +108,7 @@ const Dashboard = () => {
               <span>Send Popups</span>
             </Link>
             <Link
-              href="/offers"
+              href="/pending-offers"
               className="px-8 py-8 bg-northeasternWhite text-northeasternRed border-4 border-northeasternRed font-semibold rounded-2xl shadow-xl hover:bg-northeasternRed hover:text-northeasternWhite transition flex flex-col items-center justify-center text-center text-lg w-48 h-48"
             >
               <span className="text-4xl mb-2">🤝</span>
