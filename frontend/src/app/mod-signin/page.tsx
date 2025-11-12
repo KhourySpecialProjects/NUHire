e877cdfe0e427b21a597fa84f53b864d1cbaa294
Zaclabit <zbranden6@gmail.com>
2025-11-12 11:08:16 -0500

    fix navbar

--- frontend/src/app/mod-signin/page.tsx
+++ frontend/src/app/mod-signin/page.tsx
@@ -31,10 +31,10 @@ export default function ModeratorSignIn() {
         <Slideshow />
       </div>       
       <div className="fixed inset-0 bg-sand/80 z-5" />
-      <div className="bg-northeasternBlack text-northeasternWhite flex items-center px-6 py-4 font-rubik border-b-4 border-northeasternRed w-full">
+      <div className="z-7 bg-northeasternBlack text-northeasternWhite flex items-center px-6 py-4 font-rubik border-b-4 border-northeasternRed w-full">
         <Link
           href="/advisor-dashboard"
-          className="text-3xl font-rubik font-bold text-northeasternRed drop-shadow-lg"
+          className="text-3xl font-rubik text-center font-bold text-northeasternRed drop-shadow-lg"
         >
           NUHire
         </Link>
