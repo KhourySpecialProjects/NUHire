f06c7c6b49624cecff882a797606bfdd8ebfc2b1
Zaclabit <zbranden6@gmail.com>
2025-12-10 11:09:11 -0500

    fix reumes

--- frontend/src/app/res-review-group/page.tsx
+++ frontend/src/app/res-review-group/page.tsx
@@ -450,6 +450,7 @@ export default function ResReviewGroup() {
   const completeResumes = () => {
     if (!socket || !user) return;
     
+    console.log("Completing resume review, moving to interview stage AAAAA");
     updateProgress(user, "interview");
     localStorage.setItem("progress", "interview");
     window.location.href = "/interview-stage";
